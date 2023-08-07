2bcdbb91500e0ba838e8571b306631e585c744dc
Nguyễn Đại <nguyendai060703@gmail.com>
2023-08-07 11:26:12 +0700

    Add new work screen & fix UI

--- apps/WorkManagingApp/app/src/main/java/com/example/workmanagingapp/view/days/MyDayAdapter.kt
+++ apps/WorkManagingApp/app/src/main/java/com/example/workmanagingapp/view/days/MyDayAdapter.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.workmanagingapp.R
 import com.example.workmanagingapp.model.Day
 
@@ -15,7 +14,7 @@ class MyDayAdapter(
     private val list: MutableList<Day>
 ): RecyclerView.Adapter<MyDayViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyDayViewHolder {
-        return MyDayViewHolder(LayoutInflater.from(context).inflate(R.layout.day_layout, parent, false))
+        return MyDayViewHolder(LayoutInflater.from(context).inflate(R.layout.day_item_layout, parent, false))
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
